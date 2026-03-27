8dbdcd8aed8e1df49ac8129ae68f196c173cc7fd
q84mahdi <ghasemi84mahdi@gmail.com>
2026-03-27 20:37:46 +0330

    Update utils functions to TS

--- src/Utils/formatNumber.ts
+++ src/Utils/formatNumber.ts
@@ -1,9 +1,7 @@
 const englishDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
 
-export function toEnglishNumbersWithoutComma(n) {
-  if (typeof n !== "string" && typeof n !== "number") return "";
-
+export function toEnglishNumbersWithoutComma(n: string | number) {
   let str = n.toString();
 
   str = str.replace(/٬/g, "");
